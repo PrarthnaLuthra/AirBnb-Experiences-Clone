541f142f8f2fc4ad1868f228e35d7ba12be7b1e2
Prarthna <prarthnaluthra@gmail.com>
2022-08-24 19:32:33 +0530

    prettified

--- src/App.js
+++ src/App.js
@@ -1,52 +1,34 @@
-import './App.css';
-import React from 'react';
-import Navbar from './Navbar';
-import Hero from './Hero';
-import Header from './Header';
-import Card from './Card';
-import Activities from './Activities';
-import data from './data';
-import Heading from './Heading';
-import CardImg1 from './images/Card1.png';
-import CardImg2 from './images/Card2.png';
-import CardImg3 from './images/Card3.png';
+import "./App.css";
+import React from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Header from "./Header";
+import Card from "./Card";
+import Activities from "./Activities";
+import data from "./data";
+import Heading from "./Heading";
+import CardImg1 from "./images/Card1.png";
+import CardImg2 from "./images/Card2.png";
+import CardImg3 from "./images/Card3.png";
 
 function App() {
-  const activity = data.map(item => {
-    return (
-        <Activities
-            Key={item.id}
-            item={item}
-        />
-    )
-})        
-
+  const activity = data.map((item) => {
+    return <Activities Key={item.id} item={item} />;
+  });
 
   return (
     <>
-    <Navbar/>
-    <Hero/>
-    <Header/>
-    
-    <div className='Cards'>
-    <Card
-    img={CardImg1}
-    city="Delhi"
-    />
-    <Card
-     img={CardImg2}
-     city="Kerela"
-    />
-    <Card
-     img={CardImg3}
-     city="Mumbai"
-    />
-    </div>
-    <Heading/>
-    <section className='activities-list'>
-    {activity}
-    </section>
-    
+      <Navbar />
+      <Hero />
+      <Header />
+
+      <div className="Cards">
+        <Card img={CardImg1} city="Delhi" />
+        <Card img={CardImg2} city="Kerela" />
+        <Card img={CardImg3} city="Mumbai" />
+      </div>
+      <Heading />
+      <section className="activities-list">{activity}</section>
     </>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,10 @@
 import React from "react";
-// import Card1 from "./Card1.png"
-// import star from "./star.png";
 
-export default function Card(props){
-    
-    return(
-        <div className="Card">
-            <img src={props.img} className="CardImage"/>
-            <span className="CardCity">{props.city}</span>
-
-        </div>
-    )
+export default function Card(props) {
+  return (
+    <div className="Card">
+      <img src={props.img} className="CardImage" />
+      <span className="CardCity">{props.city}</span>
+    </div>
+  );
 }
